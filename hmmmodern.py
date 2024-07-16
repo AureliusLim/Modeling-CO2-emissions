85ebf93a03a85c132c673b35af2a0a8d6dcea70b
AureliusLim <aurelius_justin_lim@dlsu.edu.ph>
2024-07-16 22:49:40 +0800

    add modern and settings for timeframe
    -hmm not fixed

---
+++ hmmmodern.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import glob
+from hmmlearn import hmm
+import numpy as np
+import joblib
+# Path to the directory containing CSV files
+csv_files_path = 'hmmdatamodern/*.csv'
+
+# Get a list of all CSV files in the directory
+csv_files = glob.glob(csv_files_path)
+
+# Read and concatenate all CSV files into a single DataFrame
+dataframes = []
+
+for file in csv_files:
+    df = pd.read_csv(file)
+    dataframes.append(df)
+
+# Concatenate all dataframes
+combined_df = pd.concat(dataframes, ignore_index=True)
+
+# Optionally, save the combined dataframe to a new CSV file
+combined_df.to_csv('combined_data_modern.csv', index=False)
+
+print(combined_df.head())
+print(combined_df.shape)
+
+
+combined_df = combined_df.drop(columns=['Suddeness', 'TimeStamp', 'Driver Behavior', 'Reason for lane change', 'Remarks'])
+print(combined_df.isnull().sum())
+# Map passenger load to numerical values
+load_map = {'Low (0 - 33 %)': 0, 'Moderate (34 - 80 %)': 1, 'Full (81 - 100%)': 2}
+combined_df['Passenger Load'] = combined_df['Passenger Load'].map(load_map)
+
+# Convert observed and hidden states to numerical values
+hidden_states = combined_df['Hidden State'].unique()
+observed_states = combined_df['Observed State'].unique()
+
+hidden_state_map = {state: i for i, state in enumerate(hidden_states)}
+observed_state_map = {state: i for i, state in enumerate(observed_states)}
+# Print the mappings
+print("Hidden State Mapping:", hidden_state_map)
+print("Observed State Mapping:", observed_state_map)
+
+combined_df['Hidden State'] = combined_df['Hidden State'].map(hidden_state_map)
+combined_df['Observed State'] = combined_df['Observed State'].map(observed_state_map)
+
+# Split data into sequences
+sequences = []
+start_idx = 0
+
+for i in range(1, len(combined_df)):
+    if combined_df.loc[i, 'Passenger Load'] < combined_df.loc[i-1, 'Passenger Load']:  
+        sequences.append((combined_df.loc[start_idx:i, ['Observed State', 'Passenger Load']].values, 
+                          combined_df.loc[start_idx:i, 'Hidden State'].values))
+        start_idx = i
+
+sequences.append((combined_df.loc[start_idx:, ['Observed State', 'Passenger Load']].values, 
+                  combined_df.loc[start_idx:, 'Hidden State'].values))
+
+print("sequences:", sequences)
+# Split sequences into observations and hidden states
+X = [seq[0] for seq in sequences]
+Y = [seq[1] for seq in sequences]
+# Combine all sequences for fitting the model
+X_combined = np.concatenate(X)
+lengths = [len(x) for x in X]
+
+# Train a Gaussian HMM
+model = hmm.GaussianHMM(n_components=len(hidden_states), covariance_type="diag", n_iter=1000)
+model.fit(X_combined, lengths)
+
+# Print model parameters
+print("Transition matrix")
+print(model.transmat_)
+print(model.score(X_combined))
+print("\nMeans and covariances of each hidden state")
+for i in range(len(hidden_states)):
+    print("\nHidden state", i)
+    print("Mean =", model.means_[i])
+    print("Covariance matrix =\n", model.covars_[i])
+
+joblib.dump(model, 'trained_hmm_model_modern.pkl')
--- routeCombiner.py
+++ routeCombiner.py
@@ -2,11 +2,21 @@ import xml.etree.ElementTree as ET
 
 # Define the mapping of file names to prefixes
 file_prefixes = {
-    "randomTrips_car.rou.xml": "car",
-    "randomTrips_bus.rou.xml": "bus",
-    "randomTrips_truck.rou.xml": "truck",
-    "randomTrips_motorcycle.rou.xml": "motorcycle",
-    "randomTrips_bicycle.rou.xml": "bicycle"
+    "randomTrips_car1.rou.xml": "car1",
+    "randomTrips_bus1.rou.xml": "bus1",
+    "randomTrips_truck1.rou.xml": "truck1",
+    "randomTrips_motorcycle1.rou.xml": "motorcycle1",
+    "randomTrips_bicycle1.rou.xml": "bicycle1",
+    "randomTrips_car2.rou.xml": "car2",
+    "randomTrips_bus2.rou.xml": "bus2",
+    "randomTrips_truck2.rou.xml": "truck2",
+    "randomTrips_motorcycle2.rou.xml": "motorcycle2",
+    "randomTrips_bicycle2.rou.xml": "bicycle2",
+    "randomTrips_car3.rou.xml": "car3",
+    "randomTrips_bus3.rou.xml": "bus3",
+    "randomTrips_truck3.rou.xml": "truck3",
+    "randomTrips_motorcycle3.rou.xml": "motorcycle3",
+    "randomTrips_bicycle3.rou.xml": "bicycle3",
 }
 
 # Function to modify IDs in a route file
