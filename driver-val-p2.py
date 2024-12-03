e5a837d7ea3854e6031c0887d8110a0aa92cf338
AureliusLim <aurelius_justin_lim@dlsu.edu.ph>
2024-12-03 22:02:41 +0800

    fix files

--- driver-val-p2.py
+++ driver-val-p2.py
@@ -432,11 +432,11 @@ modify_person_xml(filename, trad_count, modern_count)
 if mode in [1, 2, 3]:
     # Assign configFile based on mode
     if mode == 1:
-        configFile = "configs/scenario1.sumo.cfg"
+        configFile = "configs/dashval1.sumo.cfg"
     elif mode == 2:
-        configFile = "configs/scenario2.sumo.cfg"
+        configFile = "configs/dashval2.sumo.cfg"
     elif mode == 3:
-        configFile = "configs/scenario3.sumo.cfg"
+        configFile = "configs/dashval3.sumo.cfg"
     
     # Start SUMO and connect to TraCI (assuming your existing setup)
     sumoBinary = "sumo-gui"
